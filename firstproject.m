71ee93768d4d644d78cbbd84ba26441cfe751a38
AlaaAlshemi <93677436+AlaaAlshemi@users.noreply.github.com>
2021-11-03 21:38:53 +0200

    Add files via upload

---
+++ firstproject.m
@@ -0,0 +1,26 @@
+% Alaa ahmed mohamed alshemi 
+% Group: 3A
+A=[-7 5 -9; 2 -1 2;1 -1 2 ]
+B=[16 3 2 13; 5 10 11 8; 9 6 7 12;4 15 14 1]
+C=[4 2 -3;7 -7 9;3 -5 6]
+D=[6 3 2;2 12 -7;-1 6 2;-5 15 11]
+3*A-5*C
+%7*A+2*B is error
+C*A
+C*D'
+zeros(4)
+zeros(5,4)
+ones(4)
+ones(5,4)
+size(D)
+zeros(size(D))
+diag([1 2 3 4])
+eye(5)
+%[A,B] is error
+%[A;B] is error
+%matrix 7*8
+i=[5;5;5;5;5;5;5];
+j=([5 5 5 5 5 5 5]);
+x=[i,j]
+A(2,:)
+A(:,1)
